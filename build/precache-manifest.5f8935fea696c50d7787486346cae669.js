13e7c04955dca50b5702d145fc87ec0c4608c14b
Francisco Neto <fcsneto@users.noreply.github.com>
2021-10-13 04:02:13 -0300

    modify gitignore

---
+++ build/precache-manifest.5f8935fea696c50d7787486346cae669.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163a0b46fdc95c4fc34c022282583a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4af64c7716aef50f793",
+    "url": "/static/css/2.676add4a.chunk.css"
+  },
+  {
+    "revision": "b4af64c7716aef50f793",
+    "url": "/static/js/2.d5a13e1a.chunk.js"
+  },
+  {
+    "revision": "be44a617d65c9515168c4be11964b9ac",
+    "url": "/static/js/2.d5a13e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc51d4ff1f7d6d15fdf6",
+    "url": "/static/js/main.14df8458.chunk.js"
+  },
+  {
+    "revision": "f8aaae6253b62226ba37",
+    "url": "/static/js/runtime-main.8ea5f059.js"
+  },
+  {
+    "revision": "08f39897dbee999c32caef9d8232b863",
+    "url": "/static/media/logo.08f39897.svg"
+  },
+  {
+    "revision": "cd9dc0a03aaf06f884e324efb55fef90",
+    "url": "/static/media/restaurante-fake.cd9dc0a0.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
